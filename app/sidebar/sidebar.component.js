027acf4247469a2fdfc8a03bad5455fe28f6ca3a
Eliran Koren <elirankoren@gmail.com>
2016-07-29 20:54:15 +0300

    Marked clicked toolbar button

--- app/sidebar/sidebar.component.js
+++ app/sidebar/sidebar.component.js
@@ -7,6 +7,12 @@ angular.
     controller: ['$rootScope', '$scope', '$location', 'Logger', 'Subject', 'Helper',
       function SidebarController($rootScope, $scope, $location, Logger, Subject, Helper){
         var currentPath = '';
+        var toolbarItemUnclickedClass = 'sidebar-toolbar-unclicked';
+        var toolbarItemClickedClass = 'sidebar-toolbar-clicked';
+        $scope.toolbarItemsClassObject = {
+          addItemClass: toolbarItemUnclickedClass,
+          removeItemClass: toolbarItemUnclickedClass
+        };
         $scope.subjectInfo = {};
 
         $scope.$on('$locationChangeStart', function (event) {
@@ -32,6 +38,31 @@ angular.
             });
           }
         });
+
+        var setNewToolbarItemClass = function (oldValue) {
+          return oldValue === toolbarItemUnclickedClass ? toolbarItemClickedClass : toolbarItemUnclickedClass;
+        };
+
+        var setDefaultToolbarItemsClass = function () {
+          $scope.toolbarItemsClassObject.addItemClass = toolbarItemUnclickedClass;
+          $scope.toolbarItemsClassObject.removeItemClass = toolbarItemUnclickedClass;
+        };
+
+        $scope.toolbarItemClick = function (toolbarButton) {
+          var toolbarItemOldValue = toolbarButton === 'add' ? $scope.toolbarItemsClassObject.addItemClass : $scope.toolbarItemsClassObject.removeItemClass;
+
+          setDefaultToolbarItemsClass();
+
+          switch (toolbarButton) {
+            case 'add':
+              $scope.toolbarItemsClassObject.addItemClass = setNewToolbarItemClass(toolbarItemOldValue);
+              break;
+            case 'remove':
+              $scope.toolbarItemsClassObject.removeItemClass = setNewToolbarItemClass(toolbarItemOldValue);
+              break;
+            default:
+          }
+        };
       }
     ]
   });
